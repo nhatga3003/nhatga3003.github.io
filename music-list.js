c3172a4bd8f2cb081ea749f0d77d7f4bd5f5ad91
nhatga3003 <138297438+nhatga3003@users.noreply.github.com>
2023-07-01 19:23:43 +0700

    upload

---
+++ music-list.js
@@ -0,0 +1,54 @@
+// Để thêm bài nhạc mình sẽ them vào list sau
+
+//   {
+//     name: "Tên bài nhạc",
+//     artist: "Nghệ sĩ",
+//     img: "Tên ảnh định dạng .jpg trong project",
+//     src: "Link đường dẫn file nhạc đinh dạng .mp3"
+//   }
+
+let allMusic = [
+  {
+    name: "Đáy Biển",
+    artist: "Nhất Chi Lựu Liên",
+    img: "Day-bien",
+    src: "Day-bien"
+  },
+ 
+  {
+    name: "Dũng Khí",
+    artist: "Miên Tử",
+    img: "Dong-mien",
+    src: "Dong-mien"
+  },
+  {
+    name: "Đại Thiên Bồng",
+    artist: "Lộ Gia",
+    img: "d-t-b",
+    src: "d-t-b"
+  },
+  {
+    name: "Vây Giữ",
+    artist: "Vương Tĩnh Văn Không Mập",
+    img: "v-g",
+    src: "v-g"
+  },
+  {
+    name: "Dễ Thay Đổi",
+    artist: "Vương Tĩnh Văn Không Mập",
+    img: "d-t-d",
+    src: "d-t-d"
+  },
+  {
+    name: "Nổi Gió Rồi",
+    artist: "Châu Thâm",
+    img: "noigioroi",
+    src: "noigioroi"
+  },
+  {
+    name: "Mặc Vấn Quy Kỳ",
+    artist: "Tưởng Tuyết Nhi",
+    img: "macvanquyky",
+    src: "macvanquyky"
+  },
+];
